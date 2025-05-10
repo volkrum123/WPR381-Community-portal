5c0161edc9a3d591e02ef87e7f98269cba549bf7
Jeremy K <jeremykahora3@gmail.com>
2025-05-10 21:38:31 +0200

    Added POST route for contact.ejs
    Added a POST route with a in-memory array to store user information

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const router = express.Router();
 
+const record = [];//In-memory array to store user data temporarily
+
 router.get('/', (req, res) => {
     res.render('pages/home');
 });
@@ -18,6 +20,20 @@ router.get('/events', (req, res) => {
 router.get('/contact', (req, res) => {
     res.render('pages/contact');
 });
+
+//POST route for form submission from client browser
+router.post('/contact',(req,res)=>{
+    const {name, email, message} = req.body;
+
+ //We store user information in the in-memory array   
+    record.push({
+        name,
+        email,
+        message,
+        date: new Date()
+    });
+    res.redirect('/thankyou')
+})
 
 router.get('/thankyou', (req, res) => {
     res.render('pages/thankyou');
