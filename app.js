b58af551ef4c0a1e69771b31800d0f80bd8c486c
Jeremy K <jeremykahora3@gmail.com>
2025-05-11 22:24:45 +0200

    Added code to read request body
    Code has been added to read the user details when the form is submitted

--- app.js
+++ app.js
@@ -11,9 +11,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({extended:true}));//required for req body to work
+
 app.use("/", pageRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
 
